e3777d08d8d9ac23b70412d378b8cacec3e5e320
happy <happyraj9939@gmail.com>
2024-02-07 18:39:23 +0530

    add create a post api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,89 +1,88 @@
-import bcryptjs from 'bcryptjs'
-import { errorHandler } from '../utils/error.js'
-import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import User from '../models/user.model.js';
 
-
-
-
-export const test = (req,res) =>{
-    res.json({message:'API is working!'})
-}
-
-
-export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.userId) {
-        return next(errorHandler(403, 'You are not allowed to update this user'));
-    }
-
-    try {
-        const updateFields = {};
-
-        if (req.body.password) {
-            if (req.body.password.length < 6) {
-                return next(errorHandler(400, 'Password must be at least 6 characters'));
-            }
-            updateFields.password = bcryptjs.hashSync(req.body.password, 10);
-        }
-
-        if (req.body.username) {
-            if (req.body.username.length < 7 || req.body.username.length > 20) {
-                return next(errorHandler(400, 'Username must be between 7 and 20 characters'));
-            }
-            if (req.body.username.includes(' ')) {
-                return next(errorHandler(400, 'Username cannot contain spaces'));
-            }
-            if (req.body.username !== req.body.username.toLowerCase()) {
-                return next(errorHandler(400, 'Username must be lowercase'));
-            }
-            if (!req.body.username.match(/^[a-zA-z0-9]+$/)) {
-                return next(errorHandler(400, 'Username can only contain letters and numbers'));
-            }
-            updateFields.username = req.body.username;
-        }
-
-        if (req.body.email) {
-            // Validate email format
-            // You can use a library like "validator" for email validation
-            updateFields.email = req.body.email;
-        }
-
-        if (req.body.profilePicture) {
-            updateFields.profilePicture = req.body.profilePicture;
-        }
-
-        const updateUser = await User.findByIdAndUpdate(req.params.userId, { $set: updateFields }, { new: true });
-        const { password, ...rest } = updateUser._doc;
-        res.status(200).json(rest);
-    } catch (error) {
-        next(error);
-    }
+export const test = (req, res) => {
+  res.json({ message: 'API is working!' });
 };
 
 
 
-export const deleteUser=async (req,res,next)=>{
-    if(req.user.id!==req.params.userId){
-        return next(errorHandler(403,'You are not allowed to delete this user'));
 
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this user'));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, 'Password must be at least 6 characters'));
     }
-    try {
-        await User.findByIdAndDelete(req.params.userId);
-        res.status(200).json('User has been deleted');
-        
-    } catch (error) {
-        next(error);
-        
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, 'Username must be between 7 and 20 characters')
+      );
     }
-}
-
-
-
-
-
-export const signout=(req,res,next)=>{
-    try {
-        res.clearCookie('access_token').status(200).json('User has been signed out');
-    } catch (error) {
-        next(error);
+    if (req.body.username.includes(' ')) {
+      return next(errorHandler(400, 'Username cannot contain spaces'));
     }
-}
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, 'Username must be lowercase'));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, 'Username can only contain letters and numbers')
+      );
+    }
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+ 
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json('User has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+
+
+export const signout = (req, res, next) => {
+  try {
+    res
+      .clearCookie('access_token')
+      .status(200)
+      .json('User has been signed out');
+  } catch (error) {
+    next(error);
+  }
+};
+
