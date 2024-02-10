22f11b3531f341e2169175cf72c33d08f825451e
happy <happyraj9939@gmail.com>
2024-02-10 20:56:54 +0530

    add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.routes.js';
 import postRoutes from './routes/post.routes.js';
+import commentRoutes from './routes/comment.routes.js'
 import cookieParser from 'cookie-parser';
 const app=express();
 
@@ -28,6 +29,8 @@ app.listen(3000,()=>{
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
+app.use('/api/comment',commentRoutes)
+
 
 
 
