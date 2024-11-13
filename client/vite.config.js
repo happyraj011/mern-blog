8decefde99f6f76e40e7e5ae6c1713369b785f96
happy <happyraj9939@gmail.com>
2024-11-13 12:07:40 +0530

    complete

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:3000',
-        secure:false,
-
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
       },
     },
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({ open: true }),  // Optional: for analyzing bundle size
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';  // Bundle all node_modules into a single chunk
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 2000,  // Increase the chunk size limit (optional)
+  },
+});
